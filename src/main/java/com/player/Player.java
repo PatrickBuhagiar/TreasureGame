62375cd5de34bb0f6864f083e5be8f8c8f654055
Patrick <Patrick@WindowsPC>
2014-04-22 14:11:36 +0200

    Created Player glass with generate start position

--- src/main/java/com/player/Player.java
+++ src/main/java/com/player/Player.java
@@ -5,11 +5,39 @@
  */
 
 package com.player;
-
+import com.position.Position;
+import java.util.Random;
+import com.game.Game;
+import com.map.business.tileType;
 /**
  *
  * @author Patrick
  */
 public class Player {
+    private int id;
+    private Position currentPosition;
+    private Game currentGame;
     
+    public Player(int ID, Game game){
+        id = ID;
+        currentGame = game;
+        currentPosition = generateStart();
+    }
+    
+    private Position generateStart(){
+        Random randomGenerator = new Random();
+        boolean loop = true;
+        int X=0,Y=0;
+        while (loop){
+            X = randomGenerator.nextInt(currentGame.getSize());
+            Y = randomGenerator.nextInt(currentGame.getSize());
+            //Jake Arrange this
+            if (currentGame.getMap().getTileType(X, Y) == tileType.GRASS){
+                loop = false;
+            }
+        }
+        Position currentPosition = new Position();
+        currentPosition.setPosition(X, Y);
+        return currentPosition;
+    }
 }
