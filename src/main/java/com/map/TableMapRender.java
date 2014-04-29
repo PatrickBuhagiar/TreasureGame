a9ed262d1da11b2d15d52b2fa24329d4322a7688
Jake <dalli.jake.587@gmail.com>
2014-04-29 13:37:55 +0200

    Reverting - Project isn't Building

--- src/main/java/com/map/TableMapRender.java
+++ src/main/java/com/map/TableMapRender.java
@@ -6,54 +6,16 @@
 
 package com.map;
 import com.map.business.htmlPage;
-import com.map.business.tileType;
-import com.map.htmlGenerator.*;
-
-
 /**
  *
  * @author Jake
  */
 public class TableMapRender extends htmlPage {
-    private int width, height;
-    private tileType[][] map;
-    public TableMapRender(tileType [][] mapArray, String htmlStyle, String bodyStyle){
-        this.map = mapArray;
-        this.height = mapArray.length;
-        this.width = mapArray[0].length;
+    public TableMapRender(){
+        
+    }
+    public TableMapRender(String htmlStyle, String bodyStyle){
         super.setHtmlStyle(htmlStyle);
         super.setBodyStyle(bodyStyle);
-    }
-    public String generateCode(){
-        super.htmlTag.setCSSStyle("display: table; margin: auto;");
-        super.bodyTag.setCSSStyle("display: table-cell; vertical-align: middle;");
-        Table tableTag = new Table();
-        Tr trTag;
-        Td tdTag;
-        for(int i = 0; i<= height; i++){
-            trTag = new Tr();
-            for(int j = 0; j<= width; j++){
-                tdTag = new Td();
-                switch(map[i][j]){
-                    case GRASS:
-                         tdTag.setCSSStyle("width:50px;height:50px;background-color:#0C0;padding:10px;margin:10px;");
-                        break;
-                    case UNKNOWN:
-                         tdTag.setCSSStyle("width:50px;height:50px;background-color:#999;padding:10px;margin:10px;");
-                        break;
-                    case SEA:
-                         tdTag.setCSSStyle("width:50px;height:50px;background-color:#03F;padding:10px;margin:10px;");
-                        break;
-                    case TREASURE:
-                         tdTag.setCSSStyle("width:50px;height:50px;background-color:#FF0;padding:10px;margin:10px;");
-                        break;
-                }
-                trTag.placeTag(tdTag);
-            }
-            tableTag.placeTag(trTag);
-        }
-        bodyTag.placeTag(tableTag);
-        htmlTag.placeTag(bodyTag);
-        return htmlTag.getCode();
     }
 }
