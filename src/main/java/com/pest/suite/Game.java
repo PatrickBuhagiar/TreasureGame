94021b20cc78f25f487e6820793af97fb43b4208
Jake <dalli.jake.587@gmail.com>
2014-04-29 14:48:24 +0200

    Changed Launcher Name

--- src/main/java/com/pest/suite/Game.java
+++ src/main/java/com/pest/suite/Game.java
@@ -62,6 +62,29 @@ public class Game {
         return size;
     }
     
+    private void playerInput(){
+        Scanner sc = new Scanner(System.in);
+        int varSize, varPlayers;
+        do{
+            System.out.println("Welcome! How many players are you?");
+            varPlayers = sc.nextInt();
+            if((varPlayers>=2 && varPlayers<=4)){
+                do{
+                    System.out.println("Please enter a map size between 5 and 50:");
+                    varSize = sc.nextInt();
+                }while(varSize > 5 && varSize <  50);
+            } else if((varPlayers > 5) && (varPlayers < 8)){
+                do{
+                    System.out.println("Please enter a map size between 8 and 50:");
+                    varSize = sc.nextInt();
+                }while(varSize >  5 && varSize <  50);
+            } else {
+                varSize = 50;
+            }
+        }while(varPlayers < 2 && varPlayers > 8);
+        setPlayers(varPlayers);
+        setSize(varSize);
+    }
 /**
  * Contains the main working algortihms for class
  * including input verification of number of players
@@ -69,62 +92,14 @@ public class Game {
  */
     protected void GameMethod() {
         System.out.println("Welcome! How many players are you?\n");
-        
-        //For Looping purposes 
-        boolean loop = true;
-        boolean loop2 = true;
-        
-        /**
-         * x:# of players
-         * y:Map size
-         */
-        int x=0;
-        int y=0;
-        
-        while (loop){
-            //open input stream
-            Scanner input = new Scanner(System.in);
-            //enter # of players
-            x = input.nextInt();
-            
-            //condition for 2-4 players
-            if ((x>=2)&&(x<=4)){
-                System.out.println("what map size would you like? [5-50]\n");
-                while(loop2){
-                    //input Map size  
-                    y = input.nextInt();
-                    //accepting only numbers between 5-50 to break loop 
-                    if ((y>=5) && (y<=50)){
-                        loop2=false;
-                    }
-                }
-                loop=false;
-            }
-            //condition for 5-8 players
-            else if ((x>=5)&&(x<=8)){
-                System.out.println("what map size would you like? [8-50]\n");
-                while(loop2){
-                    //input Map size  
-                    y = input.nextInt();
-                    //accepting only numbers between 5-50 to break loop 
-                    if ((y>=8) && (y<=50)){
-                        loop2=false;
-                    }
-                }
-                loop=false;
-            }
-            Map m = new Map(); // Create a Map Instance
-            m.setMapSize(y, y); // Set the Map Size
-            m.generate(); // Generates the Map 
-            ArrayList<Player> playerList = new ArrayList();
-            for(int i = 0; i<= x; i++){
-                playerList.add(new Player(i,this));
-            }
+        playerInput();
+        Map m = new Map(); // Create a Map Instance
+        m.setMapSize(size, size); // Set the Map Size
+        m.generate(); // Generates the Map 
+        ArrayList<Player> playerList = new ArrayList();
+        for(int i = 0; i<= players; i++){
+            playerList.add(new Player(i,this));
         }
-        //set class variables
-        setPlayers(x);
-        setSize(y);
-        
         //checking if correct inputs
         System.out.println("Number of players: " + getPlayers());
         System.out.println("Mapsize is: " + getSize() +"x"+getSize());
