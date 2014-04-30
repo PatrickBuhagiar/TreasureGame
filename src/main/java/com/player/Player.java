5ca793311fd8e6e9cb6381ccf8065f5cb88cbbc0
Jake <dalli.jake.587@gmail.com>
2014-04-30 03:40:21 +0200

    Removed a useless loop.

--- src/main/java/com/player/Player.java
+++ src/main/java/com/player/Player.java
@@ -150,11 +150,6 @@ public class Player {
         tmr = new TableMapRender(displayMap.getMap(),"","");
         tmr.setPlayerPoint(currentPosition);
         String code = tmr.generateCode();
-        for(int i = 0; i<= displayMap.getMap().length - 1; i++){
-            for(int j = 0; j<= displayMap.getMap()[0].length - 1; j++){
-                // System.out.println(displayMap.getMap()[i][j].name());
-            }
-        }
         System.out.println(code);
         try {
             String file = "Player " + this.getID() + ".html";
