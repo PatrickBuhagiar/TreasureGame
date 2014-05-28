3c6a0577bb75b7906392ecf80a2677cfb723ae84
Jake <dalli.jake.587@gmail.com>
2014-05-28 03:39:55 +0200

    Cleaned TableMapRender

--- src/main/java/com/map/TableMapRender.java
+++ src/main/java/com/map/TableMapRender.java
@@ -31,10 +31,6 @@ public class TableMapRender extends htmlPage {
         super.setHtmlStyle(htmlStyle);
         super.setBodyStyle(bodyStyle);
     }
-    public void setPlayerPoint(int x, int y){
-        this.playerpoint_x = x;
-        this.playerpoint_y = y;
-    }
     
       public String generateCode(){
        super.htmlTag.setCSSStyle("display: table; margin: auto;");
